f546fa31d9f44e3a6d66f64e330775db3e4e622e
gavinB579 <batesg1103@gmail.com>
2026-05-19 06:34:54 +0000

    Small change to the javascript file that handles the navbar fetching.

--- assets/navbar/navbarJS.js
+++ assets/navbar/navbarJS.js
@@ -2,7 +2,4 @@ fetch('gavinB579.github.io/assets/navbar/navbar.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar').innerHTML = data;
-    })
-    .catch(error => {
-        console.error('Error loading navbar:', error);
     });
