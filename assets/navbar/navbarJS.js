b53dbd7ff13ed2a438e559a817dbe1a502ff1ddb
gavinB579 <batesg1103@gmail.com>
2026-05-19 06:49:36 +0000

    Another change to the fetch filepath on the JS file.

--- assets/navbar/navbarJS.js
+++ assets/navbar/navbarJS.js
@@ -1,4 +1,4 @@
-fetch('gavinB579.github.io/assets/navbar/navbarHTML.html')
+fetch('assets/navbar/navbarHTML.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar').innerHTML = data;
