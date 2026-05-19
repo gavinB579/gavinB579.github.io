5cf02f988ee611ac32fe30767bbde9a2837a3907
gavinB579 <batesg1103@gmail.com>
2026-05-19 07:18:08 +0000

    Changed directory for the script on each page to represent the correct file path.

--- assets/navbar/navbarJS.js
+++ assets/navbar/navbarJS.js
@@ -1,4 +1,4 @@
-fetch('assets/navbar/navbarHTML.html')
+fetch('/assets/navbar/navbarHTML.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar').innerHTML = data;
